5f0d13f5aad01ab36054b24a7a6e269df94e54c1
ayarayarovich <106724661+ayarayarovich@users.noreply.github.com>
2023-03-27 17:16:14 +0000

    Deploying to gh-pages from @ ayarayarovich/memorial@1215f8e2fa3ddd3c85b65c5d89a5c7176d215547 🚀

---
+++ assets/catalog-61cffd49.js
@@ -0,0 +1,9 @@
+import"./common-7e6119dc.js";async function s(i){let t;if(i?t=await fetch(`http://213.189.201.225:8000/rest/filter_products/${i}/`):t=await fetch("http://213.189.201.225:8000/rest/products/"),t.ok){document.querySelector(".products-list").innerHTML="";let e=await t.json();for(let c in e){const o=e[c],r=`<div class="product col-12 col-sm-6 col-md-4">
+                            <div class="product-card">
+                              <img src="${o.image}"
+                                   alt="">
+                              <div class="product-title">
+                                ${o.title}
+                              </div>
+                            </div>
+                          </div>`;document.querySelector(".products-list").innerHTML+=r}}}async function a(){let i=await fetch("http://213.189.201.225:8000/rest/categories/");if(i.ok){document.querySelector(".categories-list").innerHTML="";let t=await i.json();for(let e in t){const c=t[e],o=`<div class="category" data-id="${c.id}">${c.__str__}</div>`;document.querySelector(".categories-list").innerHTML+=o}document.querySelectorAll(".categories-list .category").forEach(e=>{e.addEventListener("click",()=>{s(e.dataset.id)})})}}window.addEventListener("DOMContentLoaded",()=>{a(),s()});
--- assets/catalog-a0e81cb9.js
+++
@@ -1,9 +0,0 @@
-import"./common-560cb86f.js";async function s(){let e=await fetch("http://213.189.201.225:8000/rest/products/");if(e.ok){let t=await e.json();console.log(t);for(let c in t){const o=t[c];console.log(o);const i=`<div class="product col-12 col-sm-6 col-md-4">
-                            <div class="product-card">
-                              <img src="${o.image}"
-                                   alt="">
-                              <div class="product-title">
-                                ${o.title}
-                              </div>
-                            </div>
-                          </div>`;document.querySelector(".products-list").innerHTML+=i}}}window.addEventListener("DOMContentLoaded",()=>{s()});
