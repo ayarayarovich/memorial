d3ba058863c9a3ba08c1146c8faa7e33e09dbd57
ayarayarovich <106724661+ayarayarovich@users.noreply.github.com>
2023-03-28 17:43:32 +0000

    Deploying to gh-pages from @ ayarayarovich/memorial@bf9f6f66f1a5e6989d0201f39ab9a2590527a7fb 🚀

--- assets/product-89fbf4d1.js
+++
@@ -1,5 +0,0 @@
-import"./common-d92b5991.js";async function n(r){let e=await fetch(`http://213.189.201.225:8000/rest/products/${r}/`);if(e.ok){let t=await e.json();document.querySelector(".prodPg-image").innerHTML=`<img src="${t.image}" alt="">`,document.querySelector(".prodPg-info-attrs").innerHTML="",document.querySelectorAll(".prodPg-title").forEach(i=>{i.textContent=t.title});let o=t.attrs;for(let i in o){let a=o[i];document.querySelector(".prodPg-info-attrs").innerHTML+=`<li>
-              <div class="attr-title">${a.title}</div>
-              <div class="item_dots"></div>
-              <div class="attr-value">${a.value}</div>
-            </li>`}}}window.addEventListener("DOMContentLoaded",()=>{const r=new Proxy(new URLSearchParams(window.location.search),{get:(e,t)=>e.get(t)});n(r.id)});
