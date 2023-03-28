f75c73b45081679e04f8b266db0fbccf783adbc0
Айяр Халилов <super.ayar-04@yandex.ru>
2023-03-28 20:50:38 +0300

    Add page titles

--- product/product.js
+++ product/product.js
@@ -2,6 +2,9 @@ async function get_product(product_id) {
     let response = await fetch(`http://213.189.201.225:8000/rest/products/${product_id}/`);
     if (response.ok) { // если HTTP-статус в диапазоне 200-299
         let item = await response.json();
+
+        document.title = item.title;
+
         const el = ``;
         document.querySelector('.prodPg-image').innerHTML = `<img src="${item.image}" alt="">`;
         document.querySelector('.prodPg-info-attrs').innerHTML = '';
